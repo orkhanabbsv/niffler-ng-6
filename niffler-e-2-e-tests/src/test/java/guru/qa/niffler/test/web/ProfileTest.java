ccd257a4fd7127ec17f51dffb03994b3fef19c70
Orkhan Abbasov <orkhan.abbasov@pashapay.az>
2024-10-19 16:56:49 +0400

    change category test implementation

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
@@ -5,13 +5,13 @@ import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.Category;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
-import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.ProfilePage;
 import org.junit.jupiter.api.Test;
 
 @WebTest
-public class ProfileTest {
+class ProfileTest {
 
   private static final Config CFG = Config.getInstance();
 
@@ -22,13 +22,13 @@ public class ProfileTest {
       )
   )
   @Test
-  void archivedCategoryShouldPresentInCategoriesList(CategoryJson category) {
+  void archivedCategoryShouldPresentInCategoriesList(UserJson user) {
     Selenide.open(CFG.frontUrl(), LoginPage.class)
-        .successLogin("duck", "12345")
+        .successLogin(user.username(), user.testData().password())
         .checkThatPageLoaded();
 
     Selenide.open(CFG.frontUrl() + "profile", ProfilePage.class)
-        .checkArchivedCategoryExists(category.name());
+        .checkArchivedCategoryExists(user.testData().categories().getFirst().name());
   }
 
   @User(
@@ -38,12 +38,12 @@ public class ProfileTest {
       )
   )
   @Test
-  void activeCategoryShouldPresentInCategoriesList(CategoryJson category) {
+  void activeCategoryShouldPresentInCategoriesList(UserJson user) {
     Selenide.open(CFG.frontUrl(), LoginPage.class)
-        .successLogin("duck", "12345")
+        .successLogin(user.username(), user.testData().password())
         .checkThatPageLoaded();
 
     Selenide.open(CFG.frontUrl() + "profile", ProfilePage.class)
-        .checkCategoryExists(category.name());
+        .checkCategoryExists(user.testData().categories().getFirst().name());
   }
 }
